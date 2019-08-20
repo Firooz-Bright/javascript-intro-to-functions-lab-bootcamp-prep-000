a99c7d3f7baa637dcbaa3ed32fde3284bdfe6712
Firooz Mafakaheri <firooz.mafakheri@gmail.com>
2019-08-20 01:22:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ function shout(string) {
 function whisper(string){
   reurn string.toLowerCase()
 }
+}
+function logShout(string){
+  uppercase = string.toUpperCase()
+  string = uppercase;
+  console.log(uppercase);
+}
+function logWhisper(string){
+  lowercase = string.toLowerCase()
+  string = uppercase;
+  console.log(lowercase);
+}
+function sayHiToGrandma(string){
+    if (string.toLowerCase() === string){
+      return 'I can\'t hear you!';
+    }
+    if (string.toUpperCase() === string){
+      return 'YES INDEED!';
+    }
+    if (string === 'I love you, Grandma.'){
+      return 'I love you, too.';
+    }
+}
